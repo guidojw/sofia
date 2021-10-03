52110386fb39a7ee066c8c9f76c79190bd2eb925
Wilco <wilcovanbeijnum@gmail.com>
2021-10-03 17:57:46 +0200

    Revert "Update sending mail from mailgun to improvmx (#645)"
    This reverts commit 3fa2ee11bd52587942837d2dd1d7cf9f0320f1a6.

--- config/application.rb
+++ config/application.rb
@@ -38,8 +38,7 @@ module Sofia
     config.x.slack_webhook        = credentials.dig(Rails.env.to_sym, :slack_webhook) || ''
     config.x.slack_channel        = '#monitoring'
 
-    config.x.smtp_username        = credentials.dig(:production, :smtp_username)
-    config.x.smtp_password        = credentials.dig(:production, :smtp_password)
+    config.x.mailgun_api_key      = credentials.dig(Rails.env.to_sym, :mailgun_api_key)
     config.x.sentry_dsn           = credentials.dig(Rails.env.to_sym, :sentry_dsn)
     config.x.sumup_key            = credentials.dig(Rails.env.to_sym, :sumup_affiliate_key)
     config.x.healthcheck_ids      = credentials.dig(Rails.env.to_sym, :healthcheck_ids)
